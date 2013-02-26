98a8ec504502465a0ed27efdda944d37c3a46ba6
Clément <clement.desiles@telecomsante.com>
2013-02-26 18:49:35 +0100

    ReWrap module with easier bindings

---
+++ binding.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+/******************************************************************************
+ *                                                                            *
+ * @file test.js                                                              *
+ * @author Clément Désiles <clement.desiles@telecomsante.com>                 *
+ * @date 2013/02/13                                                           *
+ * @licence MIT                                                               *
+ *                                                                            *
+ * Sample to test the nodejs bindings to PCSC                                 *
+ *                                                                            *
+ *****************************************************************************/
+
+// Script requirements
+// ****************************************************************************
+var util = require('util');
+var events = require('events');
+var fork = require('child_process').fork;
+var path = require('path');
+var node_pcsc = path.join(__dirname, 'lib/node-pcsc.js');
+
+var Bindings = function() {
+	// Call super constructor		
+	events.EventEmitter.call( this );
+
+	// PCSC sub-process reference
+	this.pcsc = null;
+};
+
+util.inherits(Bindings, events.EventEmitter);
+
+
+/**
+ * Init the pcsc sub-process
+ * @return {Boolean} operation status
+ */
+Bindings.prototype.init = function() {
+	// Protection against multiple init
+	if (this.pcsc) return false;
+
+	// Fork the sub-process
+	this.pcsc = fork(node_pcsc);
+
+	// Message appropriation
+	var self = this;
+	this.pcsc.on('message', function(message) {
+		switch (message.name) {
+			case 'readerStateChange':
+				self.emit('evt', message);	
+				break;
+		};
+	});
+
+	// Ack
+	return true;
+};
+
+/**
+ * Kill the pcsc sub-process
+ * @return {Boolean} operation status
+ */
+Bindings.prototype.release = function() {
+	if (this.pcsc) {
+		this.pcsc.kill('SIGTERM');
+	}
+	this.pcsc = null;
+	return true;
+};
+
+// Export an instance
+module.exports = new Bindings();
---
+++ example2.js
@@ -0,0 +1,13 @@
+//
+// How to bind node-pcsc from an external program
+//
+
+var pcsc = require('node-pcsc');
+pcsc.init();
+
+pcsc.on('evt', function(evt) {
+	console.log('Event status:',evt.reader.status);
+});
+
+// Release context after 5s
+setTimeout( pcsc.release, 5000);
